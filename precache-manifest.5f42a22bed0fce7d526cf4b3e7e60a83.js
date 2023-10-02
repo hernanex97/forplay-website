481ae22db2f5ff06f70eceaf1bedc36e0bbd9543
hernanex97 <expositohernan@ibm.com>
2023-10-02 16:13:30 -0300

    Updates

---
+++ precache-manifest.5f42a22bed0fce7d526cf4b3e7e60a83.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bd23eb9df146be0cc82",
+    "url": "/static/css/main.d795febf.chunk.css"
+  },
+  {
+    "revision": "5bd23eb9df146be0cc82",
+    "url": "/static/js/main.2670cd08.chunk.js"
+  },
+  {
+    "revision": "88802c931bcdd198d5fd",
+    "url": "/static/js/runtime~main.9dac7ca6.js"
+  },
+  {
+    "revision": "2348c00d2a3a76d4d361",
+    "url": "/static/css/2.d8931cb4.chunk.css"
+  },
+  {
+    "revision": "2348c00d2a3a76d4d361",
+    "url": "/static/js/2.a5ac21b4.chunk.js"
+  },
+  {
+    "revision": "9a435c87b8d7506e1f33",
+    "url": "/static/js/3.0ea8b817.chunk.js"
+  },
+  {
+    "revision": "32152ae1361134bfa5d84d952664f94b",
+    "url": "/static/media/nav-icon1.32152ae1.svg"
+  },
+  {
+    "revision": "935140a4ddf61387cccd9b4b6ed4652b",
+    "url": "/static/media/new-logoforplay-2.935140a4.png"
+  },
+  {
+    "revision": "85e85996cd9a1f3fb5ef80ced35fed35",
+    "url": "/static/media/new-color-sharp.85e85996.png"
+  },
+  {
+    "revision": "c9b960470acfe8412d453802d58bef54",
+    "url": "/static/media/new-color-sharp2.c9b96047.png"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "45625b8b3c5fbb1734f8b5cbb3ed0981",
+    "url": "/static/media/new-footer-bg.45625b8b.png"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "39f19edeee8fa5048970e7540d7274ae",
+    "url": "/index.html"
+  }
+];
